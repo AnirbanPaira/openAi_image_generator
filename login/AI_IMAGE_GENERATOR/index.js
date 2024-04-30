f67c2b5f57ff85819f5ccbda210f2f11209e3bc2
anirban_paira <anirban.paira@yoctotta.com>
2024-04-30 13:07:49 +0530

    frontend integrated

--- login/AI_IMAGE_GENERATOR/index.js
+++ login/AI_IMAGE_GENERATOR/index.js
@@ -1,13 +1,15 @@
+const path = require('path')
 const express = require ('express');
+const coros = require('cors')
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT|| 5000;
 
 const app = express();
-
+app.use(coros())
 //enable body parser
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/openai',require('./routes/openai_routes'))
 
